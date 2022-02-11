d388c4dca72628b3252d56dbfdf478ca30523914
Summer Liu <summer.ycliu@gmail.com>
2022-02-11 10:46:26 +0800

    Updates

---
+++ precache-manifest.f3b2eaa0e7c63cd2c2869d531fb4ec3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9d09c633fc93d10c5b2bc3894104e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af59939f4bffd4bf0d35",
+    "url": "/static/css/main.a8db4500.chunk.css"
+  },
+  {
+    "revision": "0ad96c9ca2ee5999350f",
+    "url": "/static/js/2.4b746741.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.4b746741.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af59939f4bffd4bf0d35",
+    "url": "/static/js/main.3d7a672e.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "da5aba01b00f1409356f914c818766af",
+    "url": "/static/media/profile.da5aba01.jpg"
+  }
+]);
